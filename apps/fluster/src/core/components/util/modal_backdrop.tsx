9f8aad99db671c2aa4c413dcc1203ec23519aabb
Fluster.IO <fluster.eyeoh@gmail.com>
2025-05-24 15:18:55 -0500

    confirmation modal pretty much wired up and working.

--- apps/fluster/src/core/components/util/modal_backdrop.tsx
+++ apps/fluster/src/core/components/util/modal_backdrop.tsx
@@ -1,4 +1,4 @@
-import { type ReactNode } from "react";
+import { useEffect, type ReactNode } from "react";
 
 interface ModalBackdropProps {
     onClick: () => void;
@@ -9,9 +9,20 @@ const ModalBackdrop = ({
     onClick,
     children,
 }: ModalBackdropProps): ReactNode => {
+    const handleKeyPress = (e: KeyboardEvent): void => {
+        if (e.key === "Escape") {
+            onClick();
+        }
+    };
+    useEffect(() => {
+        window.addEventListener("keydown", handleKeyPress);
+        return () => window.removeEventListener("keydown", handleKeyPress);
+    }, []);
     return (
         <div
-            className="w-screen h-screen fixed top-0 left-0 right-0 bottom-0 flex flex-col justify-center item-center gap-6 bg-black/30 dark:bg-black/70"
+            className={
+                "w-screen h-screen fixed top-0 left-0 right-0 bottom-0 flex flex-col justify-center items-center gap-6 bg-black/30 dark:bg-black/70"
+            }
             onClick={(e) => {
                 e.preventDefault();
                 e.stopPropagation();
--- apps/fluster/src/features/confirmation_modal/presentation/main.tsx
+++ apps/fluster/src/features/confirmation_modal/presentation/main.tsx
@@ -1,17 +1,10 @@
-import {
-    AlertDialog,
-    AlertDialogFooter,
-    AlertDialogHeader,
-    AlertDialogTrigger,
-    AlertDialogAction,
-    AlertDialogCancel,
-    AlertDialogContent,
-    AlertDialogDescription,
-    AlertDialogTitle,
-} from "@/components/ui/shad/alert-dialog";
-import { useEventListener } from "@/hooks/use_event_listener";
-import React, { useEffect, useState, type ReactNode } from "react";
 import { ConfirmationModalProps } from "../types/confirmation_modal_types";
+import ModalBackdrop from "@/components/util/modal_backdrop";
+import { Button } from "@/components/ui/shad/button";
+import { motion } from "motion/react";
+import { type ReactNode, useEffect, useState } from "react";
+import { H4 } from "@/components/typography/typography";
+import { sendConfirmationResponse } from "../data/events/confirmation_response";
 
 const ConfirmationModal = (
     props: ConfirmationModalProps & { handleClose: () => void }
@@ -21,34 +14,60 @@ const ConfirmationModal = (
         /// Produces the transition while keeping this out of the dom when not in use.
         setOpen(true);
     }, []);
+    const closeModal = (): void => {
+        setOpen(false);
+        if (props.handleClose) {
+            props.handleClose();
+        }
+    };
     return (
-        <AlertDialog
-            open={open}
-            onOpenChange={(newOpen) => {
-                console.log("newOpen: ", newOpen);
-                setOpen(newOpen);
-                if (newOpen === false) {
-                    props.handleClose();
-                }
-            }}
-        >
-            <AlertDialogContent>
-                <AlertDialogHeader>
-                    <AlertDialogTitle>{props.title}</AlertDialogTitle>
-                    <AlertDialogDescription>{props.body}</AlertDialogDescription>
-                </AlertDialogHeader>
-                <AlertDialogFooter>
+        <ModalBackdrop onClick={closeModal}>
+            <motion.div
+                className="max-w-[min(600px,90vw)] border rounded p-4 flex flex-col justify-center items-start gap-6 bg-popover text-foreground"
+                onClick={(e) => {
+                    e.stopPropagation();
+                }}
+                variants={{
+                    closed: {
+                        y: -200,
+                        opacity: 0,
+                    },
+                    opened: {
+                        y: 0,
+                        opacity: 1,
+                    },
+                }}
+                animate={open ? "opened" : "closed"}
+            >
+                <div>
+                    <H4>{props.title}</H4>
+                    <div>{props.body}</div>
+                </div>
+                <div className="w-full flex flex-row justify-end items-center gap-6">
                     {props.denyButtonText && (
-                        <AlertDialogCancel onClick={() => { }}>
+                        <Button
+                            variant={"secondary"}
+                            onClick={() => {
+                                sendConfirmationResponse(false, props.id);
+                                closeModal();
+                            }}
+                        >
                             {props.denyButtonText}
-                        </AlertDialogCancel>
+                        </Button>
                     )}
                     {props.acceptButtonText && (
-                        <AlertDialogAction>{props.acceptButtonText}</AlertDialogAction>
+                        <Button
+                            onClick={() => {
+                                sendConfirmationResponse(true, props.id);
+                                closeModal();
+                            }}
+                        >
+                            {props.acceptButtonText}
+                        </Button>
                     )}
-                </AlertDialogFooter>
-            </AlertDialogContent>
-        </AlertDialog>
+                </div>
+            </motion.div>
+        </ModalBackdrop>
     );
 };
 
