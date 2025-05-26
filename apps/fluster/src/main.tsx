32e5cc5182f0afced3a65e6c7f565cef2137956e
Fluster.IO <fluster.eyeoh@gmail.com>
2025-05-26 16:02:40 -0500

    Fix scroll overflow issue.

--- apps/fluster/src/main.tsx
+++ apps/fluster/src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/themes.scss";
 import "./styles/global.scss";
+import "./styles/math.scss";
 import App from "./App";
 import ReduxProvider from "@/state/redux_provider";
 import { DesktopScaffoldProvider } from "#/scaffold/state/scaffold_provider.tsx";
