1be039a976447297ddce15cd14b3b9a83841d6a9
Fluster.IO <uhlittlelessdumb@gmail.com>
2025-05-30 15:56:14 -0500

    Fix render error.

--- apps/fluster/src/features/snippets/presentation/add_snippet_panel/add_snippet_panel.tsx
+++ apps/fluster/src/features/snippets/presentation/add_snippet_panel/add_snippet_panel.tsx
@@ -1,12 +1,12 @@
 import SidePanelContainer from "@/components/side_panel_container";
 import {
-    Form,
-    FormControl,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
-    TagInput,
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  TagInput,
 } from "@fluster.io/dev";
 import { Input } from "@fluster.io/dev";
 import React, { useEffect, useState, type ReactNode } from "react";
@@ -20,219 +20,224 @@ import { connect } from "react-redux";
 import { useMatch, useSearchParams } from "react-router";
 import { AppRoutes } from "#/router/data/app_routes";
 import { Button } from "@fluster.io/dev";
-import { commands, SnippetItem } from "@/lib/bindings";
+import { commands, SnippetModel } from "@/lib/bindings";
 
 const snippetSchema = z.object({
-    lang: z.string(),
-    label: z
-        .string()
-        .min(2, "Your label needs to be at least 2 characters long."),
-    body: z.string().min(3, "Please add a body to this snippet."),
-    desc: z.string(),
-    tags: z.string().array(),
-    id: z.number().int().nullable(),
+  lang: z.string(),
+  label: z
+    .string()
+    .min(2, "Your label needs to be at least 2 characters long."),
+  body: z.string().min(3, "Please add a body to this snippet."),
+  desc: z.string(),
+  tags: z.string().array(),
+  ctime: z.string().date(),
+  utime: z.string().date(),
+  id: z.string().uuid().nullish(),
 });
 
 const connector = connect((state: AppState) => ({
-    panelOpen: state.panelLeft.open,
+  panelOpen: state.panelLeft.open,
 }));
 
 const AddSnippetPanel = connector(
-    ({ panelOpen }: { panelOpen: boolean }): ReactNode => {
-        const form = useForm({
-            resolver: zodResolver(snippetSchema),
-            defaultValues: {
-                label: "",
-                lang: "python",
-                body: "",
-                desc: "",
-                tags: [],
-                id: null,
-            },
+  ({ panelOpen }: { panelOpen: boolean }): ReactNode => {
+    const now = new Date().toUTCString();
+    const form = useForm({
+      resolver: zodResolver(snippetSchema),
+      defaultValues: {
+        label: "",
+        lang: "python",
+        body: "",
+        desc: "",
+        tags: [],
+        ctime: now,
+        utime: now,
+        id: null,
+      },
+    });
+    const isSnippetsPage = useMatch(AppRoutes.snippets);
+    const [editingIdState, setEditingIdState] = useState<string | null>(null);
+    const [searchParams] = useSearchParams();
+
+    useEffect(() => {
+      const editingId = searchParams.get("editing");
+      if (!editingId) {
+        return;
+      }
+      const getSnippetBeingEdited = async (id: string): Promise<void> => {
+        const res = await commands.getSnippetById(id);
+        if (res.status === "ok") {
+          const snippetItem = res.data[0];
+          const tags = res.data[1];
+          form.setValue("label", snippetItem.label);
+          form.setValue("id", snippetItem.id);
+          form.setValue("lang", snippetItem.lang);
+          form.setValue("body", snippetItem.body);
+          form.setValue("desc", snippetItem.desc ?? "");
+          form.setValue("utime", snippetItem.utime ?? now);
+          form.setValue("ctime", snippetItem.ctime ?? now);
+          form.setValue(
+            "tags",
+            tags.map((t) => t.value)
+          );
+          /* form.setValue("tags", res.data.tags); */
+        }
+      };
+
+      if (isSnippetsPage && editingId) {
+        setEditingIdState(editingId);
+        getSnippetBeingEdited(editingId).catch(() => {
+          console.error(
+            "Fluster attempted to find a snippet that you intended to edit and was unsuccessful."
+          );
         });
-        const isSnippetsPage = useMatch(AppRoutes.snippets);
-        const [editingIdState, setEditingIdState] = useState<number | null>(null);
-        const [searchParams] = useSearchParams();
+      }
+    }, [searchParams, isSnippetsPage, form, editingIdState]);
 
-        useEffect(() => {
-            const editingIdString = searchParams.get("editing");
-            if (!editingIdString) {
-                return;
-            }
-            const editingId = parseInt(editingIdString);
-            if (editingIdState === editingId || `${editingId}` !== editingIdString) {
-                return;
-            }
-            const getSnippetBeingEdited = async (id: number): Promise<void> => {
-                const res = await commands.getSnippetById(id);
-                if (res.status === "ok") {
-                    const snippetItem = res.data[0];
-                    const tags = res.data[1];
-                    form.setValue("label", snippetItem.label);
-                    form.setValue("id", snippetItem.id);
-                    form.setValue("lang", snippetItem.lang);
-                    form.setValue("body", snippetItem.body);
-                    form.setValue("desc", snippetItem.desc);
-                    form.setValue(
-                        "tags",
-                        tags.map((t) => t.value)
-                    );
-                    /* form.setValue("tags", res.data.tags); */
+    useEffect(() => {
+      if (isSnippetsPage && panelOpen) {
+        document.getElementById("snippet-name-input")?.focus();
+      }
+    }, [panelOpen, isSnippetsPage]);
+
+    const handleSubmit = async (
+      data: z.infer<typeof snippetSchema>
+    ): Promise<void> => {
+      const snippetModel: SnippetModel = {
+        id: null,
+        label: data.label,
+        body: data.body,
+        desc: data.desc,
+        lang: data.lang,
+        ctime: data.ctime,
+        utime: data.utime,
+      };
+      const res = await commands.saveSnippets([snippetModel], [data.tags]);
+      console.log("res: ", res);
+      if (res.status === "ok") {
+        form.reset();
+        window.dispatchEvent(new CustomEvent("reload-snippet-list", {}));
+      }
+    };
+
+    const exitEditingMode = () => {
+      setEditingIdState(null);
+      searchParams.delete("editing");
+      form.reset();
+    };
+
+    return (
+      <SidePanelContainer label="Add a snippet">
+        <Form {...form}>
+          <form
+            className="w-full max-w-[600px] flex flex-col justify-start items-center gap-6"
+            onSubmit={form.handleSubmit(async (_data) => handleSubmit(_data))}
+          >
+            <FormField
+              control={form.control}
+              name={"label"}
+              render={({ field }) => {
+                return (
+                  <FormItem className="w-full max-w-[600px]">
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <div className="w-full max-w-[600px]">
+                        <Input
+                          id={"snippet-name-input"}
+                          value={field.value}
+                          onChange={(e) =>
+                            form.setValue("label", e.target.value)
+                          }
+                        />
+                        <FormMessage />
+                      </div>
+                    </FormControl>
+                  </FormItem>
+                );
+              }}
+            />
+            <FormField
+              control={form.control}
+              name={"lang"}
+              render={({ field }) => {
+                return (
+                  <FormItem className="w-full max-w-[600px]">
+                    <FormLabel>Language</FormLabel>
+                    <FormControl>
+                      <div className="w-full max-w-[600px]">
+                        <FormMessage />
+                        <SyntaxSupportedLanguageSelect
+                          value={field.value}
+                          onChange={(newVal) =>
+                            form.setValue(field.name, newVal)
+                          }
+                          classes={{
+                            button: "w-full max-w-[600px]",
+                            popover: "w-full",
+                            popoverContainer: "w-full max-w-[600px]",
+                          }}
+                        />
+                      </div>
+                    </FormControl>
+                  </FormItem>
+                );
+              }}
+            />
+            <TagInput
+              form={form}
+              name="tags"
+              classes={{
+                formItem: "w-full max-w-[600px]",
+              }}
+              desc="Click on a tag to remove it."
+            />
+            <FormField
+              control={form.control}
+              name={"body"}
+              render={({ field }) => {
+                return (
+                  <FormItem className="w-full max-w-[600px]">
+                    <FormLabel>Snippet Content</FormLabel>
+                    <FormControl>
+                      <div className="w-full max-w-[600px]">
+                        <Textarea
+                          value={field.value}
+                          onChange={(e) =>
+                            form.setValue(field.name, e.target.value)
+                          }
+                          rows={8}
+                        />
+                        <FormMessage />
+                      </div>
+                    </FormControl>
+                  </FormItem>
+                );
+              }}
+            />
+            <div className="w-full flex flex-row justify-end items-center gap-6">
+              {editingIdState && (
+                <Button variant={"destructive"} onClick={exitEditingMode}>
+                  Cancel
+                </Button>
+              )}
+              <Button
+                type="submit"
+                disabled={
+                  !(
+                    form.watch("lang")?.length > 0 &&
+                    form.watch("body")?.length > 0 &&
+                    form.watch("label")?.length > 0
+                  )
                 }
-            };
-
-            if (isSnippetsPage && editingId) {
-                setEditingIdState(editingId);
-                getSnippetBeingEdited(editingId).catch(() => {
-                    console.error(
-                        "Fluster attempted to find a snippet that you intended to edit and was unsuccessful."
-                    );
-                });
-            }
-        }, [searchParams, isSnippetsPage, form, editingIdState]);
-
-        useEffect(() => {
-            if (isSnippetsPage && panelOpen) {
-                document.getElementById("snippet-name-input")?.focus();
-            }
-        }, [panelOpen, isSnippetsPage]);
-
-        const handleSubmit = async (
-            data: z.infer<typeof snippetSchema>
-        ): Promise<void> => {
-            const snippetItem: SnippetItem = {
-                id: null,
-                label: data.label,
-                body: data.body,
-                desc: data.desc,
-                lang: data.lang,
-            };
-            const res = await commands.saveSnippet(snippetItem, data.tags);
-            console.log("res: ", res);
-            if (res.status === "ok") {
-                form.reset();
-                window.dispatchEvent(new CustomEvent("reload-snippet-list", {}));
-            }
-        };
-
-        const exitEditingMode = () => {
-            setEditingIdState(null);
-            searchParams.delete("editing");
-            form.reset();
-        };
-
-        return (
-            <SidePanelContainer label="Add a snippet">
-                <Form {...form}>
-                    <form
-                        className="w-full max-w-[600px] flex flex-col justify-start items-center gap-6"
-                        onSubmit={form.handleSubmit(async (_data) => handleSubmit(_data))}
-                    >
-                        <FormField
-                            control={form.control}
-                            name={"label"}
-                            render={({ field }) => {
-                                return (
-                                    <FormItem className="w-full max-w-[600px]">
-                                        <FormLabel>Name</FormLabel>
-                                        <FormControl>
-                                            <>
-                                                <Input
-                                                    id={"snippet-name-input"}
-                                                    value={field.value}
-                                                    onChange={(e) =>
-                                                        form.setValue("label", e.target.value)
-                                                    }
-                                                />
-                                                <FormMessage />
-                                            </>
-                                        </FormControl>
-                                    </FormItem>
-                                );
-                            }}
-                        />
-                        <FormField
-                            control={form.control}
-                            name={"lang"}
-                            render={({ field }) => {
-                                return (
-                                    <FormItem className="w-full max-w-[600px]">
-                                        <FormLabel>Language</FormLabel>
-                                        <FormControl>
-                                            <>
-                                                <FormMessage />
-                                                <SyntaxSupportedLanguageSelect
-                                                    value={field.value}
-                                                    onChange={(newVal) =>
-                                                        form.setValue(field.name, newVal)
-                                                    }
-                                                    classes={{
-                                                        button: "w-full max-w-[600px]",
-                                                        popover: "w-full",
-                                                        popoverContainer: "w-full max-w-[600px]",
-                                                    }}
-                                                />
-                                            </>
-                                        </FormControl>
-                                    </FormItem>
-                                );
-                            }}
-                        />
-                        <TagInput
-                            form={form}
-                            name="tags"
-                            classes={{
-                                formItem: "w-full max-w-[600px]",
-                            }}
-                            desc="Click on a tag to remove it."
-                        />
-                        <FormField
-                            control={form.control}
-                            name={"body"}
-                            render={({ field }) => {
-                                return (
-                                    <FormItem className="w-full max-w-[600px]">
-                                        <FormLabel>Snippet Content</FormLabel>
-                                        <FormControl>
-                                            <>
-                                                <FormMessage />
-                                                <Textarea
-                                                    value={field.value}
-                                                    onChange={(e) =>
-                                                        form.setValue(field.name, e.target.value)
-                                                    }
-                                                    rows={8}
-                                                />
-                                            </>
-                                        </FormControl>
-                                    </FormItem>
-                                );
-                            }}
-                        />
-                        <div className="w-full flex flex-row justify-end items-center gap-6">
-                            {editingIdState && (
-                                <Button variant={"destructive"} onClick={exitEditingMode}>
-                                    Cancel
-                                </Button>
-                            )}
-                            <Button
-                                type="submit"
-                                disabled={
-                                    !(
-                                        form.watch("lang")?.length > 0 &&
-                                        form.watch("body")?.length > 0 &&
-                                        form.watch("label")?.length > 0
-                                    )
-                                }
-                            >
-                                {editingIdState ? "Update" : "Create"}
-                            </Button>
-                        </div>
-                    </form>
-                </Form>
-            </SidePanelContainer>
-        );
-    }
+              >
+                {editingIdState ? "Update" : "Create"}
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </SidePanelContainer>
+    );
+  }
 );
 
 AddSnippetPanel.displayName = "AddSnippetPanel";
