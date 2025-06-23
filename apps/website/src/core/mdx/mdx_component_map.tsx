fcb2ed90a005cbe336721f1e612cb57dc437711b
Fluster.IO <uhlittlelessdumb@gmail.com>
2025-06-23 15:25:52 -0500

    Website in much better position with footer and resume added along with some small doc changes.

---
+++ apps/website/src/core/mdx/mdx_component_map.tsx
@@ -0,0 +1,18 @@
+import { Hint } from "@fluster.io/dev";
+import defaultMdxComponents from "fumadocs-ui/mdx";
+import type { MDXComponents } from "mdx/types";
+import { ReactNode } from "react";
+export function getMDXComponents(components?: MDXComponents): MDXComponents {
+    return {
+        ...defaultMdxComponents,
+        ...components,
+        blockquote: (props: { children: ReactNode }) => {
+            return (
+                <div className={"pl-4 border-l-4 border-l-primary [&_p]:before:content-none [&_p]:after:content-none"}>
+                    {props.children}
+                </div>
+            );
+        },
+        Hint: Hint,
+    };
+}
