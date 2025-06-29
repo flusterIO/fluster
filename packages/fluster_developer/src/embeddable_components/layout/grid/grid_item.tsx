e95f66546c115a17bd9b0d5bac7250891047436a
Fluster.IO <uhlittlelessdumb@gmail.com>
2025-06-29 12:15:54 -0500

    Moving on to jupyter integration.

--- packages/fluster_developer/src/embeddable_components/layout/grid/grid_item.tsx
+++ packages/fluster_developer/src/embeddable_components/layout/grid/grid_item.tsx
@@ -2,15 +2,30 @@ import React, { HTMLProps, type ReactNode } from "react";
 import { cn } from "../../../utils/cn";
 
 interface GridItemProps extends HTMLProps<HTMLDivElement> {
-  children: ReactNode;
+    children: ReactNode;
+    border?: boolean;
+    center?: boolean;
 }
 
-export const GridItem = ({ children, ...props }: GridItemProps): ReactNode => {
-  return (
-    <div {...props} className={cn("w-full h-full rounded", props.className)}>
-      {children}
-    </div>
-  );
+export const GridItem = ({
+    children,
+    border,
+    center,
+    ...props
+}: GridItemProps): ReactNode => {
+    return (
+        <div
+            {...props}
+            className={cn(
+                "w-full h-full",
+                border && "border rounded",
+                center && "flex flex-col justify-center items-center",
+                props.className
+            )}
+        >
+            {children}
+        </div>
+    );
 };
 
 GridItem.displayName = "GridItem";
