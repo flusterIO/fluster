d992e8de6bf56caa46590a1e4de6e68c0766cfef
Fluster.IO <uhlittlelessdumb@gmail.com>
2025-07-01 15:20:20 -0500

    Code blocks in much better shape.

--- apps/fluster/src/main.tsx
+++ apps/fluster/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "@fluster.io/dev/themes.scss";
 import "./styles/global.scss";
 import "./styles/math.scss";
+import "./styles/mdx.scss";
 import App from "./App";
 import ReduxProvider from "@/state/redux_provider";
 import { DesktopScaffoldProvider } from "#/scaffold/state/scaffold_provider.tsx";
