7d9a469354fcfa9e7a9d796da6fe926f6725af32
Fluster.IO <uhlittlelessdumb@gmail.com>
2025-07-04 10:38:03 -0500

    another attempt at fixing github action

--- apps/fluster/src/features/mdx/presentation/wrapped_components/plots/line_plot.tsx
+++ apps/fluster/src/features/mdx/presentation/wrapped_components/plots/line_plot.tsx
@@ -1,19 +1,13 @@
 import { useDarkMode } from "@/hooks/use_dark_mode";
-import { LinePlotComponent, LinePlotProps } from "@fluster.io/dev";
-import React, { type ReactNode } from "react";
-import { InlineMdxContent } from "../../inline_mdx_content";
+import { type ReactNode } from "react";
+/* import { InlineMdxContent } from "../../inline_mdx_content"; */
 
-export const WrappedLinePlot = (
-    props: Omit<LinePlotProps, "darkMode" | "InlineMdxContent">
-): ReactNode => {
-    const darkMode = useDarkMode();
-    return (
-        <LinePlotComponent
-            darkMode={darkMode}
-            InlineMdxContent={InlineMdxContent}
-            {...props}
-        />
-    );
-};
+export const WrappedLinePlot =
+    (): /* props: Omit<LinePlotProps, "darkMode" | "InlineMdxContent"> */
+        ReactNode => {
+        const darkMode = useDarkMode();
+        console.log("darkMode: ", darkMode);
+        return null;
+    };
 
 WrappedLinePlot.displayName = "WrappedLinePlot";
