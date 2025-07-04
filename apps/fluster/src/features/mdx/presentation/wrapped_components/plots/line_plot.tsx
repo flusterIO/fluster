eefba26436f7d41369d7f198f1956a6ec308f9aa
Fluster.IO <uhlittlelessdumb@gmail.com>
2025-07-04 10:33:31 -0500

    another attempt at fixing github action

--- apps/fluster/src/features/mdx/presentation/wrapped_components/plots/line_plot.tsx
+++ apps/fluster/src/features/mdx/presentation/wrapped_components/plots/line_plot.tsx
@@ -1,19 +1,19 @@
-import { type ReactNode } from "react";
+import { useDarkMode } from "@/hooks/use_dark_mode";
+import { LinePlotComponent, LinePlotProps } from "@fluster.io/dev";
+import React, { type ReactNode } from "react";
+import { InlineMdxContent } from "../../inline_mdx_content";
 
 export const WrappedLinePlot = (
-    props: object
-    /* props: Omit<LinePlotProps, "darkMode" | "InlineMdxContent"> */
+    props: Omit<LinePlotProps, "darkMode" | "InlineMdxContent">
 ): ReactNode => {
-    console.log("props: ", props);
-    /* const darkMode = useDarkMode(); */
-    return null;
-    /* return ( */
-    /*     <LinePlotComponent */
-    /*         darkMode={darkMode} */
-    /*         InlineMdxContent={InlineMdxContent} */
-    /*         {...props} */
-    /*     /> */
-    /* ); */
+    const darkMode = useDarkMode();
+    return (
+        <LinePlotComponent
+            darkMode={darkMode}
+            InlineMdxContent={InlineMdxContent}
+            {...props}
+        />
+    );
 };
 
 WrappedLinePlot.displayName = "WrappedLinePlot";
