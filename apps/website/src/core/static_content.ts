a92f38208419e60cc4b06e456c7f54f0379d77ff
Fluster.IO <uhlittlelessdumb@gmail.com>
2025-07-10 09:42:43 -0500

    Giving up on the updater for now to focus on more important things. Will come back to this when the app is in a better place

--- apps/website/src/core/static_content.ts
+++ apps/website/src/core/static_content.ts
@@ -5,37 +5,36 @@
 const baseUrl = "https://fluster.vercel.app";
 
 export const staticContent = {
-  links: {
-    comingSoon: "/coming_soon",
-    sponsor: "/sponsor",
-    social: {
-      github: "https://github.com/igloo1505/ulld",
-      twitter: "https://x.com/uhlittlelessdum",
-      discord: "https://discord.gg/FUA88wwqUM",
+    links: {
+        comingSoon: "/coming_soon",
+        sponsor: "/sponsor",
+        social: {
+            github: "https://github.com/igloo1505/ulld",
+            twitter: "https://x.com/uhlittlelessdum",
+            discord: "https://discord.gg/FUA88wwqUM",
+        },
+        github: {
+            releases: "https://github.com/flusterIO/fluster/releases",
+        },
+        fund: {
+            paypalDonate: "https://www.paypal.com/ncp/payment/D6S6NP4AHJD6Y",
+            patreon: "https://www.patreon.com/uhlittlelessdum/about",
+            github: "https://github.com/igloo1505/ulld",
+        },
+        docs: {
+            internal: {
+                userHome: baseUrl,
+                organization_docs: baseUrl,
+            },
+            external: {
+                jupyter: "jupyter-notebook.readthedocs.io",
+                python: "docs.python.org",
+                cslRepo: "https://github.com/citation-style-language/styles",
+                monaco: "https://microsoft.github.io/monaco-editor",
+            },
+        },
+        videoDemo: "https://www.youtube.com/watch?v=zzeE1sIphe0&t=4s",
     },
-    github: {
-      releases: "https://github.com/flusterIO/fluster/releases",
-    },
-    fund: {
-      paypalDonate:
-        "https://www.paypal.com/donate/?hosted_button_id=W22RTUJ2RPZDU",
-      patreon: "https://www.patreon.com/uhlittlelessdum/about",
-      github: "https://github.com/igloo1505/ulld",
-    },
-    docs: {
-      internal: {
-        userHome: baseUrl,
-        organization_docs: baseUrl,
-      },
-      external: {
-        jupyter: "jupyter-notebook.readthedocs.io",
-        python: "docs.python.org",
-        cslRepo: "https://github.com/citation-style-language/styles",
-        monaco: "https://microsoft.github.io/monaco-editor",
-      },
-    },
-    videoDemo: "https://www.youtube.com/watch?v=zzeE1sIphe0&t=4s",
-  },
 } as const;
 
 export type StaticWebsiteContent = typeof staticContent;
