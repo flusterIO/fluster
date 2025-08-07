896e0e559105667001dc9e8c9db1d09f13f00ecd
Fluster.IO <uhlittlelessdumb@gmail.com>
2025-08-07 10:34:22 -0500

    Push to release npm packages

--- scripts/release_npm.zsh
+++ scripts/release_npm.zsh
@@ -2,6 +2,7 @@ cd $FLUSTER_NATIVE_ROOT
 pnpm changeset
 pnpm changeset version
 make local_deps_to_workspace_version
+pnpm install
 git add --all
 git commit -m "Push to release npm packages"
 git push
