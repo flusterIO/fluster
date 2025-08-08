a3fab95868cb71938034ab9b80dce1f242ac3197
Fluster.IO <uhlittlelessdumb@gmail.com>
2025-08-08 15:28:51 -0500

    Add 'mywork' section to new website, but article is not yet written.

--- apps/website/src/app/myWork/page.tsx
+++ apps/website/src/app/myWork/page.tsx
@@ -12,13 +12,13 @@ const MyWorkPage = async (): Promise<ReactNode> => {
 
     const MDX = page.data.body;
     return (
-        <div className="w-full py-16 flex flex-col justify-start items-center">
+        <div className="w-full pb-16 flex flex-col justify-start items-center">
             <div className="px-4 py-3 w-full flex flex-row justify-start items-center">
                 <Link href="/" className="text-bold">
                     Fluster
                 </Link>
             </div>
-            <div className="prose dark:prose-invert prose-code:before:content-none prose-code:after:content-none prose-code:bg-[--shiki-light-bg] dark:prose-code:bg-[--shiki-dark-bg] [&_code_*]:text-[--shiki-light] dark:[&_code_*]:text-[--shiki-dark] max-w-[min(90vw,1080px)]">
+            <div className="mt-16 prose dark:prose-invert prose-code:before:content-none prose-code:after:content-none prose-code:bg-[--shiki-light-bg] dark:prose-code:bg-[--shiki-dark-bg] [&_code_*]:text-[--shiki-light] dark:[&_code_*]:text-[--shiki-dark] max-w-[min(90vw,1080px)]">
                 <MDX components={getMDXComponents()} />
             </div>
         </div>
