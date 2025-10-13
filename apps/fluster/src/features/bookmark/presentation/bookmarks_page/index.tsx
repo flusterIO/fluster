870a8f5b29afe1e7a8c645321ab258be3b2c3701
Fluster.IO <uhlittlelessdumb@gmail.com>
2025-10-13 08:17:03 -0500

    Add sort by last_read to bookmarks page.

--- apps/fluster/src/features/bookmark/presentation/bookmarks_page/index.tsx
+++ apps/fluster/src/features/bookmark/presentation/bookmarks_page/index.tsx
@@ -4,43 +4,55 @@ import { commands, MdxBookmarkData } from "@/lib/bindings";
 import React, { useEffect, useState, type ReactNode } from "react";
 import { BookmarkList } from "./bookmark_list";
 import { useEventListener } from "@fluster.io/dev";
+import dayjs from "dayjs";
 
 declare global {
-  interface WindowEventMap {
-    "request-bookmark-list-refresh": CustomEvent<object>;
-  }
+    interface WindowEventMap {
+        "request-bookmark-list-refresh": CustomEvent<object>;
+    }
 }
 
 const BookmarksPage = (): ReactNode => {
-  const [bookmarks, setBookmarks] = useState<MdxBookmarkData[] | null>(null);
-  const getBookmarks = async (): Promise<void> => {
-    const res = await commands.getBookmarkedNotes();
-    if (res.status === "ok") {
-      setBookmarks(res.data);
-    } else {
-      setBookmarks([]);
-      console.error("An error occurred while gathering your bookmarks.");
-    }
-  };
-  useEffect(() => {
-    getBookmarks();
-  }, []);
+    const [bookmarks, setBookmarks] = useState<MdxBookmarkData[] | null>(null);
+    const getBookmarks = async (): Promise<void> => {
+        const res = await commands.getBookmarkedNotes();
+        if (res.status === "ok") {
+            setBookmarks(
+                res.data.sort((a, b) => {
+                    return (
+                        dayjs(b.note.last_read, {
+                            utc: true,
+                        }).valueOf() -
+                        dayjs(a.note.last_read, {
+                            utc: true,
+                        }).valueOf()
+                    );
+                })
+            );
+        } else {
+            setBookmarks([]);
+            console.error("An error occurred while gathering your bookmarks.");
+        }
+    };
+    useEffect(() => {
+        getBookmarks();
+    }, []);
 
-  useEventListener("request-bookmark-list-refresh", () => {
-    getBookmarks();
-  });
+    useEventListener("request-bookmark-list-refresh", () => {
+        getBookmarks();
+    });
 
-  return (
-    <PanelContainer className="pt-16">
-      {bookmarks === null ? (
-        <div className="w-full h-full min-h-[calc(100vh-6rem)] flex flex-col justify-center items-center">
-          <LoadingComponent />
-        </div>
-      ) : (
-        <BookmarkList items={bookmarks} />
-      )}
-    </PanelContainer>
-  );
+    return (
+        <PanelContainer className="pt-16">
+            {bookmarks === null ? (
+                <div className="w-full h-full min-h-[calc(100vh-6rem)] flex flex-col justify-center items-center">
+                    <LoadingComponent />
+                </div>
+            ) : (
+                <BookmarkList items={bookmarks} />
+            )}
+        </PanelContainer>
+    );
 };
 
 BookmarksPage.displayName = "BookmarksPage";
