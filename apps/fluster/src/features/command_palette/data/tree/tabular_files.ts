d0888e9504ee25ac119cef8557f32d10bf144aaa
Fluster.IO <uhlittlelessdumb@gmail.com>
2025-10-15 17:30:30 -0500

    Fix dashboard responsiveness

--- apps/fluster/src/features/command_palette/data/tree/tabular_files.ts
+++ apps/fluster/src/features/command_palette/data/tree/tabular_files.ts
@@ -1,13 +1,14 @@
 import { CommandPaletteAnyEntry } from "../models/command_palette_any_entry";
 import { CommandPaletteCategory } from "../models/command_palette_category";
-import { GeneralCommandPaletteItem } from "../models/command_palette_item";
-import { getPdfUrl } from "#/pdf/data/utils/get_pdf_url";
-import { NavigateFunction } from "react-router";
-import store from "@/state/store";
-import { AppState } from "@/state/initial_state";
+// import { GeneralCommandPaletteItem } from "../models/command_palette_item";
+// import { getPdfUrl } from "#/pdf/data/utils/get_pdf_url";
+// import { NavigateFunction } from "react-router";
+// import store from "@/state/store";
+// import { AppState } from "@/state/initial_state";
 import { ReactNode } from "react";
+// import { commands } from "@/lib/bindings";
 
-export class PdfFilesCommandPaletteRoot extends CommandPaletteCategory {
+export class TabularFilesCommandPaletteRoot extends CommandPaletteCategory {
     constructor() {
         super("Tabular Files", "tabular");
     }
@@ -18,18 +19,23 @@ export class PdfFilesCommandPaletteRoot extends CommandPaletteCategory {
         return null;
     }
     async getItems(): Promise<CommandPaletteAnyEntry[]> {
-        const res = await getFilesByFileExtensions(["csv"]);
-        const notesDir = (store.getState() as AppState).core.notesDirectory ?? "";
-        return res.map((s) => {
-            const item = new GeneralCommandPaletteItem(
-                notesDir.length && s.startsWith(notesDir) ? s.replace(notesDir, "") : s,
-                `pdf-${s}`,
-                async (nav: NavigateFunction) => {
-                    nav(getPdfUrl(s));
-                }
-            );
-            item.itemClasses = "text-sm [&_p]:text-sm";
-            return item;
-        });
+        // const notesDir = (store.getState() as AppState).core.notesDirectory ?? "";
+        // const res = await commands.getFilesByFileExtensions(
+        //     ["csv"],
+        //     notesDir,
+        //     32
+        // );
+        // return res.map((s) => {
+        //     const item = new GeneralCommandPaletteItem(
+        //         notesDir.length && s.startsWith(notesDir) ? s.replace(notesDir, "") : s,
+        //         `pdf-${s}`,
+        //         async (nav: NavigateFunction) => {
+        //             nav(getPdfUrl(s));
+        //         }
+        //     );
+        //     item.itemClasses = "text-sm [&_p]:text-sm";
+        //     return item;
+        // });
+        return [];
     }
 }
