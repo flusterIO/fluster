87d1b1bdb2e2e39e647d83187cda9e7a6104e0e2
Fluster.IO <uhlittlelessdumb@gmail.com>
2025-10-15 21:39:26 -0500

    Fix img-src to allow gltf models.

--- apps/fluster/src/features/command_palette/data/tree/tabular_files.ts
+++ apps/fluster/src/features/command_palette/data/tree/tabular_files.ts
@@ -1,12 +1,13 @@
 import { CommandPaletteAnyEntry } from "../models/command_palette_any_entry";
 import { CommandPaletteCategory } from "../models/command_palette_category";
-// import { GeneralCommandPaletteItem } from "../models/command_palette_item";
-// import { getPdfUrl } from "#/pdf/data/utils/get_pdf_url";
-// import { NavigateFunction } from "react-router";
-// import store from "@/state/store";
-// import { AppState } from "@/state/initial_state";
+import { GeneralCommandPaletteItem } from "../models/command_palette_item";
+import { getTabularDataTableUrl } from "@/lib/url_utils";
+import { NavigateFunction } from "react-router";
+import store from "@/state/store";
+import { AppState } from "@/state/initial_state";
 import { ReactNode } from "react";
-// import { commands } from "@/lib/bindings";
+import { commands } from "@/lib/bindings";
+import { showToast } from "@fluster.io/dev";
 
 export class TabularFilesCommandPaletteRoot extends CommandPaletteCategory {
     constructor() {
@@ -19,23 +20,34 @@ export class TabularFilesCommandPaletteRoot extends CommandPaletteCategory {
         return null;
     }
     async getItems(): Promise<CommandPaletteAnyEntry[]> {
-        // const notesDir = (store.getState() as AppState).core.notesDirectory ?? "";
-        // const res = await commands.getFilesByFileExtensions(
-        //     ["csv"],
-        //     notesDir,
-        //     32
-        // );
-        // return res.map((s) => {
-        //     const item = new GeneralCommandPaletteItem(
-        //         notesDir.length && s.startsWith(notesDir) ? s.replace(notesDir, "") : s,
-        //         `pdf-${s}`,
-        //         async (nav: NavigateFunction) => {
-        //             nav(getPdfUrl(s));
-        //         }
-        //     );
-        //     item.itemClasses = "text-sm [&_p]:text-sm";
-        //     return item;
-        // });
-        return [];
+        const coreState: AppState["core"] = (store.getState() as AppState).core;
+        const res = await commands.getFilesByFileExtensions(
+            ["csv"],
+            coreState.notesDirectory,
+            coreState.nThreads.toString()
+        );
+        if (res.status === "error") {
+            showToast({
+                title: "An error occurred",
+                body: "An error occurred while attempting to gather your tabular files. If this continues, please file an issue on Github.",
+                variant: "Error",
+                duration: 5000,
+            });
+            return [];
+        }
+        return res.data.map((s) => {
+            const item = new GeneralCommandPaletteItem(
+                coreState.notesDirectory.length &&
+                    s.startsWith(coreState.notesDirectory)
+                    ? s.replace(coreState.notesDirectory, "")
+                    : s,
+                `tabular-${s}`,
+                async (nav: NavigateFunction) => {
+                    nav(getTabularDataTableUrl(s));
+                }
+            );
+            item.itemClasses = "text-sm [&_p]:text-sm";
+            return item;
+        });
     }
 }
