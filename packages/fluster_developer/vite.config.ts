dcc7f8565e547ae0b32eb17f0eef8084b947f951
Fluster.IO <uhlittlelessdumb@gmail.com>
2025-12-07 15:59:48 -0600

    Add paper back to website

--- packages/fluster_developer/vite.config.ts
+++ packages/fluster_developer/vite.config.ts
@@ -5,32 +5,33 @@ import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    dts({
-      insertTypesEntry: true,
-      copyDtsFiles: true,
-    }),
-  ],
-  clearScreen: false,
-  build: {
-    lib: {
-      entry: resolve(__dirname, "src/index.ts"),
-      formats: ["es"],
-      name: "fluster",
-      fileName: (c) => `index.${c}.js`,
-      cssFileName: "themes",
+    plugins: [
+        react(),
+        /* @ts-expect-error -- Not sure what it's complaining about... it works. */
+        dts({
+            insertTypesEntry: true,
+            copyDtsFiles: true,
+        }),
+    ],
+    clearScreen: false,
+    build: {
+        lib: {
+            entry: resolve(__dirname, "src/index.ts"),
+            formats: ["es"],
+            name: "fluster",
+            fileName: (c) => `index.${c}.js`,
+            cssFileName: "themes",
+        },
+        commonjsOptions: { transformMixedEsModules: true },
+        rollupOptions: {
+            external: ["react", "react-dom", "tailwindcss"],
+        },
+        sourcemap: true,
+        emptyOutDir: false,
     },
-    commonjsOptions: { transformMixedEsModules: true },
-    rollupOptions: {
-      external: ["react", "react-dom", "tailwindcss"],
+    test: {
+        environment: "jsdom",
+        globals: true,
+        setupFiles: "./tests/setup.js",
     },
-    sourcemap: true,
-    emptyOutDir: false,
-  },
-  test: {
-    environment: "jsdom",
-    globals: true,
-    setupFiles: "./tests/setup.js",
-  },
 });
